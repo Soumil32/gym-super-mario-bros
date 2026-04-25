fa23dd7b82956579c3520ec73a7e8f2b29ac48ef
Soumil Gupta <104227845+Soumil32@users.noreply.github.com>
2026-04-25 23:27:43 +0100

    Fix import statement for smb_env module

--- gym_super_mario_bros/smb_env_straight.py
+++ gym_super_mario_bros/smb_env_straight.py
@@ -1,4 +1,4 @@
-import smb_env
+import .smb_env
 class SuperMarioBrosEnvStraight(smb_env.SuperMarioBrosEnv):
     """This environment focuses on just getting Mario form the start of the level to the end of level
     in the 'traditional' way which is going from the right of the map to left of map whilst trying not to die"""
