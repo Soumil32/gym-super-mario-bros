d259a4233d0ecc65d288146fba78cbabe3643357
Soumil Gupta <104227845+Soumil32@users.noreply.github.com>
2026-04-26 20:42:48 +0100

    Add SuperMarioBrosEnvStraight to package exports

--- gym_super_mario_bros/__init__.py
+++ gym_super_mario_bros/__init__.py
@@ -1,6 +1,7 @@
 """Registration code of Gym environments in this package."""
 from .smb_env import SuperMarioBrosEnv
 from .smb_random_stages_env import SuperMarioBrosRandomStagesEnv
+from .smb_env_straight import SuperMarioBrosEnvStraight
 from ._registration import make
 
 
@@ -9,4 +10,5 @@ __all__ = [
     make.__name__,
     SuperMarioBrosEnv.__name__,
     SuperMarioBrosRandomStagesEnv.__name__,
+    SuperMarioBrosEnvStraight.__name
 ]
