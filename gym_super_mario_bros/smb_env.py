72c9ed06950c9e9f908d6c14f9b4a9c8d61c9b8d
Soumil Gupta <104227845+Soumil32@users.noreply.github.com>
2026-05-07 22:53:24 +0100

    Fix x_position calculation for accuracy

--- gym_super_mario_bros/smb_env.py
+++ gym_super_mario_bros/smb_env.py
@@ -141,7 +141,7 @@ class SuperMarioBrosEnv(NESEnv):
     def _x_position(self):
         """Return the current horizontal position."""
         # add the current page 0x6d to the current x
-        return int(self.ram[0x6d] * 0x100) + int(self.ram[0x86])
+        return int(int(self.ram[0x6d]) * 0x100) + int(self.ram[0x86])
 
     @property
     def _left_x_position(self):
